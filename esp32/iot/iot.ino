4950f3a8ac02767b0846f0e80ea28f20b7f7d459
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2025-10-28 13:46:49 +0700

    add arduino code

---
+++ esp32/iot/iot.ino
@@ -0,0 +1,89 @@
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPL6hambC-OO"
+#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
+#define BLYNK_AUTH_TOKEN            "Azcllw0pB0dkBmvXpZHUA_vEvwNR3U38"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "PERSANDIAN";
+char pass[] = "51b3rm4n";
+
+BlynkTimer timer;
+
+// Pin untuk soil sensor
+const int SOIL_SENSOR_PIN = 32;
+int soilMoisture = 0;
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+
+  // Update state
+  Blynk.virtualWrite(V1, value);
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+  // Change Web Link Button message to "Congratulations!"
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  // Baca nilai soil sensor dari pin 32
+  soilMoisture = analogRead(SOIL_SENSOR_PIN);
+  
+  // Kirim nilai soil sensor ke Blynk V2
+  Blynk.virtualWrite(V2, soilMoisture);
+  
+  // Tampilkan di Serial Monitor
+  Serial.print("Soil Moisture: ");
+  Serial.println(soilMoisture);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  
+  // Setup pin soil sensor sebagai input
+  pinMode(SOIL_SENSOR_PIN, INPUT);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
+
